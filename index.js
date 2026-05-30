047282f5013269600fe69c7bb6f33e2065f49d04
ACalebN <Adam@nettleship-group.com>
2026-05-30 14:25:21 +0100

    added functions

--- index.js
+++ index.js
@@ -11,3 +11,33 @@
 'use strict';
 
 // add your functions here
+
+function replaceText(elem, str) {
+    elem.textContent = str
+}
+
+function addTextTo(elem, str) {
+    elem.textContent = elem.textContent + str
+}
+
+function moreBears() {
+    let bear = document.querySelector("#animals")
+    bear.src = 'http://placebear.com/400/200'
+    bear.alt = 'A bear.'
+    bear.title = 'A BEAR!'
+}
+
+function setId(elem, str) {
+    elem.id = str
+    return elem
+}
+
+function setClass(elem, str) {
+    elem.className  = str
+    return elem
+}
+
+function addAClass(elem, str) {
+    
+    return elem
+}
